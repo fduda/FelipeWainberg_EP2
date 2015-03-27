45560c53d3d0d55d8794738f4a12165cb5815907
fduda <felipeduda1@gmail.com>
2015-03-27 16:59:12 -0300

    Lista
    Nesse commit foi importado o arquivo entrada.txt, transformado em lista,
    removido todo o "lixo" das palavras, alem de ter sido removida a linha
    em branco

--- Forca.py
+++ Forca.py
@@ -5,9 +5,20 @@ Created on Fri Mar 27 15:41:26 2015
 @author: Duda
 """
 
+import turtle
+import random
+#from FuncoesForca import *
+
+f=open('entrada.txt', encoding="utf-8")
+
+l2=f.readlines()
+l=[]
+for w in l2:
+    l.append(w.strip())
+    
+l.remove('')
+keyword=random.choice(l)
 
 
-from FuncoesForca import *
-
-
+print(l)
 
